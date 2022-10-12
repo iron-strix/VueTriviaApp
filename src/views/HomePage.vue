b84b6fede1bf1ef82bce8640e8e63a974f0539d4
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-10-12 01:27:32 -0500

    main page layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,7 +3,39 @@ import { ref } from 'vue'
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">I'm Home Page</h1>
-  </main>
+  <div class="brand">
+    <img class="logo" src="logo.svg" alt="logo" />
+    <h1 class="title">Vue Trivia App</h1>
+    <img class="logo" src="logo.svg" alt="logo" />
+  </div>
+  <div class="categories">
+    <RouterLink v-for="n in 24" :key="n" :to="`/question/category/${n}`" class="category"
+      >Category - {{ n }}</RouterLink
+    >
+  </div>
 </template>
+
+<style class="postcss" scoped>
+.brand {
+  @apply flex items-center justify-center gap-4;
+  & .logo {
+    @apply h-16 w-16;
+  }
+
+  & .title {
+    @apply text-5xl uppercase tracking-wide text-gray-200;
+  }
+}
+
+.categories {
+  @apply grid flex-grow grid-cols-4 gap-12;
+
+  & .category {
+    @apply flex h-32 w-40 items-center justify-center rounded-lg border-4 border-solid border-gray-600 py-4 font-bold uppercase text-gray-800 transition-colors duration-300;
+
+    &:hover {
+      @apply cursor-pointer border-gray-900 bg-slate-500 text-white;
+    }
+  }
+}
+</style>
